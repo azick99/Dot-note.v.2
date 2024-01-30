742a941e20174b825dcebec42b41a53d8b514678
azick99 <yunusaliyevazizbek80@gmail.com>
2024-01-30 19:03:28 +0100

    setting chat functionality and basic stuff

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from '@/components/ChatWrapper'
+import ChatWrapper from '@/components/chat/ChatWrapper'
 import PdfRenderer from '@/components/PdfRenderer'
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
@@ -12,12 +12,12 @@ interface PageProps {
 
 const Page = async ({ params }: PageProps) => {
   const { fileid } = params
+
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
   if (!user || !user?.id) redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
-  // make database call
   const file = await db.file.findFirst({
     where: {
       id: fileid,
@@ -28,16 +28,18 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound()
 
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh - 3.5rem)]">
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-        {/* left side bar */}
+        {/* Left sidebar & main wrapper */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer url={file.url}/>
+            {/* Main area */}
+            <PdfRenderer url={file.url} />
           </div>
         </div>
-        <div className="shrink-0 flex-[0.75] boder-t border-gray-200 lg:w-96 lg:border-l border-t-0">
-          <ChatWrapper />
+
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
