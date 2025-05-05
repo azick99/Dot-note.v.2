4dd6eb1ab4a458f395ec7535b5962ab37415a20f
Azizbek <yunusaliyevazizbek80@gmail.com>
2025-05-05 02:33:51 +0200

    refactor: update experimental settings in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false
     config.resolve.alias.encoding = false
     return config
   },
 }
-
 
 module.exports = nextConfig
